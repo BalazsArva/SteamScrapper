5c325c35ac1264924ef71f9d46eb0903b06ae2c2
Árva Balázs <balazs.arva@windowslive.com>
2021-02-21 16:14:12 +0100

    Small refactorings in Crawler. A WIP implementation for splitting the exploration set and explored set into smaller ones based on link types.

--- SteamScrapper/SteamScrapper/Crawler.cs
+++ SteamScrapper/SteamScrapper/Crawler.cs
@@ -93,15 +93,14 @@ namespace SteamScrapper
 
                 var steamPage = await steamPageFactory.CreateSteamPageAsync(addressToProcessUri);
 
-                var toBeExploredLinks = steamPage.NormalizedLinks.Where(uri => IsLinkAllowedForExploration(uri)).Select(uri => new RedisValue(uri.AbsoluteUri)).ToArray();
-                var ignoredLinks = steamPage.NormalizedLinks.Where(uri => !IsLinkAllowedForExploration(uri)).Select(uri => new RedisValue(uri.AbsoluteUri)).ToArray();
-
                 var updateExplorationStatusTransaction = redisDatabase.CreateTransaction();
+                var toBeExploredLinks = steamPage.NormalizedLinks.Where(uri => IsLinkAllowedForExploration(uri)).Select(uri => new RedisValue(uri.AbsoluteUri)).ToArray();
                 var helperSetId = $"Crawler:{redisKeyDateStamp}:HelperSets:{Guid.NewGuid():n}";
 
                 var addToBeExploredToHelperSetTask = updateExplorationStatusTransaction.SetAddAsync(helperSetId, toBeExploredLinks);
                 if (enableLoggingIgnoredLinks)
                 {
+                    var ignoredLinks = steamPage.NormalizedLinks.Where(uri => !IsLinkAllowedForExploration(uri)).Select(uri => new RedisValue(uri.AbsoluteUri)).ToArray();
                     var addIgnoredLinksTask = updateExplorationStatusTransaction.SetAddAsync($"Crawler:{redisKeyDateStamp}:Ignored", ignoredLinks);
                 }
 
@@ -182,7 +181,54 @@ namespace SteamScrapper
 
         private async Task<bool> TryRegisterLinkAsExploredAsync(string redisKeyDateStamp, Uri addressToProcessUri)
         {
-            return await redisDatabase.SetAddAsync($"Crawler:{redisKeyDateStamp}:Explored", addressToProcessUri.AbsoluteUri);
+            // TODO: These are not yet usable, because the set difference used in the DiscoverSteamLinksAsync method relies on a single 'Explored' set.
+            /*
+            const int bitmapSize = 1024;
+
+            var absoluteUri = addressToProcessUri.AbsoluteUri;
+
+            if (absoluteUri.StartsWith(PageUrlPrefixes.App, StringComparison.OrdinalIgnoreCase))
+            {
+                var appId = Utilities.LinkHelpers.SteamLinkHelper.ExtractAppId(addressToProcessUri);
+
+                var bitmapId = appId / bitmapSize;
+                var bitmapOffset = appId % bitmapSize;
+
+                return await redisDatabase.StringSetBitAsync($"Crawler:{redisKeyDateStamp}:Explored:AppBitmaps:{bitmapId}", bitmapOffset, true);
+            }
+
+            if (absoluteUri.StartsWith(PageUrlPrefixes.Sub, StringComparison.OrdinalIgnoreCase))
+            {
+                var subId = Utilities.LinkHelpers.SteamLinkHelper.ExtractSubId(addressToProcessUri);
+
+                var bitmapId = subId / bitmapSize;
+                var bitmapOffset = subId % bitmapSize;
+
+                return await redisDatabase.StringSetBitAsync($"Crawler:{redisKeyDateStamp}:Explored:SubBitmaps:{bitmapId}", bitmapOffset, true);
+            }
+
+            if (absoluteUri.StartsWith(PageUrlPrefixes.Bundle, StringComparison.OrdinalIgnoreCase))
+            {
+                var bundleId = Utilities.LinkHelpers.SteamLinkHelper.ExtractBundleId(addressToProcessUri);
+
+                var bitmapId = bundleId / bitmapSize;
+                var bitmapOffset = bundleId % bitmapSize;
+
+                return await redisDatabase.StringSetBitAsync($"Crawler:{redisKeyDateStamp}:Explored:BundleBitmaps:{bitmapId}", bitmapOffset, true);
+            }
+
+            if (absoluteUri.StartsWith(PageUrlPrefixes.Developer, StringComparison.OrdinalIgnoreCase))
+            {
+                return await redisDatabase.SetAddAsync($"Crawler:{redisKeyDateStamp}:Explored:Developer", addressToProcessUri.AbsoluteUri);
+            }
+
+            if (absoluteUri.StartsWith(PageUrlPrefixes.Publisher, StringComparison.OrdinalIgnoreCase))
+            {
+                return await redisDatabase.SetAddAsync($"Crawler:{redisKeyDateStamp}:Explored:Publisher", addressToProcessUri.AbsoluteUri);
+            }
+            */
+
+            return await redisDatabase.SetAddAsync($"Crawler:{redisKeyDateStamp}:Explored:EverythingElse", addressToProcessUri.AbsoluteUri);
         }
 
         private async Task<Uri> GetNextUriToProcessAsync(string redisKeyDateStamp)
