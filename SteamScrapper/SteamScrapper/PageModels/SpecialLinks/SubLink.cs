489d0bac48bf13b202d8195617f7dfb951c295ae
Árva Balázs <balazs.arva@windowslive.com>
2021-02-21 12:32:27 +0100

    Created immutable DTOs for special links: - App - Sub - Bundle

---
+++ SteamScrapper/SteamScrapper/PageModels/SpecialLinks/SubLink.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using SteamScrapper.Utilities.LinkHelpers;
+
+namespace SteamScrapper.PageModels.SpecialLinks
+{
+    public class SubLink
+    {
+        public SubLink(Uri address)
+        {
+            if (address is null)
+            {
+                throw new ArgumentNullException(nameof(address));
+            }
+
+            Address = address;
+            SubId = SteamLinkHelper.ExtractSubId(address);
+        }
+
+        public Uri Address { get; }
+
+        public int SubId { get; }
+    }
+}
