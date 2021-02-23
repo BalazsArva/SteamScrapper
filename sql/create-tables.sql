630d727fb7c48a4d3419fa923b6b0878f5c3626f
Árva Balázs <balazs.arva@windowslive.com>
2021-02-23 20:45:07 +0100

    SQL scripts for creating basic tables and to query count of apps/subs/bundles.

---
+++ sql/create-tables.sql
@@ -0,0 +1,47 @@
+USE [SteamScrapper]
+GO
+/****** Object:  Table [dbo].[Apps]    Script Date: 2021-02-23 20:42:06 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Apps](
+	[Id] [int] NOT NULL,
+	[UtcDateTimeRecorded] [datetime2](7) NOT NULL,
+	[Title] [nvarchar](max) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Bundles]    Script Date: 2021-02-23 20:42:06 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Bundles](
+	[Id] [int] NOT NULL,
+	[UtcDateTimeRecorded] [datetime2](7) NOT NULL,
+	[Title] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_Bundles] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Subs]    Script Date: 2021-02-23 20:42:06 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Subs](
+	[Id] [int] NOT NULL,
+	[UtcDateTimeRecorded] [datetime2](7) NOT NULL,
+	[Title] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Subs] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
---
+++ sql/show-item-count.sql
@@ -0,0 +1,9 @@
+SELECT
+	SUM([All].[AppCount]) AS [AppCount],
+	SUM([All].[SubCount]) AS [SubCount],
+	SUM([All].[BundleCount]) AS [BundleCount]
+FROM (
+	SELECT COUNT([A].[Id]) AS [AppCount],               0 AS [SubCount],               0 AS [BundleCount] FROM [dbo].[Apps] AS [A] UNION
+	SELECT               0 AS [AppCount], COUNT([S].[Id]) AS [SubCount],               0 AS [BundleCount] FROM [dbo].[Subs] AS [S] UNION
+	SELECT               0 AS [AppCount],               0 AS [SubCount], COUNT([B].[Id]) AS [BundleCount] FROM [dbo].[Bundles] AS [B]
+) AS [All]
