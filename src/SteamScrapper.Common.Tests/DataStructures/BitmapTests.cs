4ba1873fe91571ea18a7a90a08b655d1ebdef4bb
Árva Balázs <balazs.arva@windowslive.com>
2021-02-25 19:06:29 +0100

    Created a bitmap datastructure and tests for it.

---
+++ src/SteamScrapper.Common.Tests/DataStructures/BitmapTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using NUnit.Framework;
+using SteamScrapper.Common.DataStructures;
+
+namespace SteamScrapper.Common.Tests.DataStructures
+{
+    public class BitmapTests
+    {
+        [TestCase(-1)]
+        [TestCase(0)]
+        public void New_CapacityIsTooSmall_ThrowsArgumentException(int capacity)
+        {
+            var exceptionThrown = Assert.Throws<ArgumentException>(() => new Bitmap(capacity));
+
+            Assert.AreEqual("capacity", exceptionThrown.ParamName);
+        }
+
+        [Test]
+        public void Get_BitmapNeverTouched_NoBitIsSet()
+        {
+            const int capacity = 3000;
+
+            var bitmap = new Bitmap(capacity);
+
+            for (var i = 0; i < capacity; ++i)
+            {
+                Assert.IsFalse(bitmap.Get(i), $"Expected the bit at index {i} to be 0, but it was 1.");
+            }
+        }
+
+        [TestCase(-10, 10)]
+        [TestCase(-1, 10)]
+        [TestCase(10, 10)]
+        [TestCase(110, 10)]
+        public void Set_IndexOutOfRange_ThrowsArgumentOutOfRangeException(int index, int capacity)
+        {
+            var bitmap = new Bitmap(capacity);
+
+            var exceptionThrown = Assert.Throws<ArgumentOutOfRangeException>(() => bitmap.Set(index, true));
+
+            Assert.AreEqual("index", exceptionThrown.ParamName);
+        }
+
+        [Test]
+        public void Set_IndexWithinRange_BitIsCorrectlySet()
+        {
+            const int capacity = 3000;
+
+            var bitmap = new Bitmap(capacity);
+
+            // True round
+            // Set and check every bit individually
+            for (var i = 0; i < capacity; ++i)
+            {
+                bitmap.Set(i, true);
+
+                Assert.IsTrue(bitmap.Get(i), $"Expected the bit at index {i} to be 1, but it was 0.");
+            }
+
+            // Re-check everything to ensure that subsequent operations don't do some bad logic, negating previous operation(s).
+            for (var i = 0; i < capacity; ++i)
+            {
+                Assert.IsTrue(bitmap.Get(i), $"Expected the bit at index {i} to be 1, but it was 0.");
+            }
+
+            // False round
+            // Set and check every bit individually
+            for (var i = 0; i < capacity; ++i)
+            {
+                bitmap.Set(i, false);
+
+                Assert.IsFalse(bitmap.Get(i), $"Expected the bit at index {i} to be 0, but it was 1.");
+            }
+
+            // Re-check everything to ensure that subsequent operations don't do some bad logic, negating previous operation(s).
+            for (var i = 0; i < capacity; ++i)
+            {
+                Assert.IsFalse(bitmap.Get(i), $"Expected the bit at index {i} to be 0, but it was 1.");
+            }
+        }
+    }
+}
---
+++ src/SteamScrapper.Common/DataStructures/Bitmap.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace SteamScrapper.Common.DataStructures
+{
+    public class Bitmap
+    {
+        private const int bucketSize = sizeof(ulong);
+
+        private readonly ulong[] buckets;
+        private readonly int size;
+
+        public Bitmap(int capacity)
+        {
+            if (capacity < 1)
+            {
+                throw new ArgumentException($"The '{nameof(capacity)}' parameter must be at least 1.", nameof(capacity));
+            }
+
+            var bucketCount = capacity / bucketSize;
+
+            size = capacity;
+            buckets = new ulong[bucketCount];
+        }
+
+        public void Set(int index, bool bit)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"The '{nameof(index)}' parameter must be at least 0.");
+            }
+
+            if (index >= size)
+            {
+                throw new ArgumentOutOfRangeException(nameof(index), $"The '{nameof(index)}' parameter must not exceed the bitmap's capacity.");
+            }
+
+            var bucketIndex = index / bucketSize;
+            var bucketOffset = index % bucketSize;
+
+            if (bit)
+            {
+                var mask = ((ulong)1) << bucketOffset;
+
+                buckets[bucketIndex] |= mask;
+            }
+            else
+            {
+                var mask1 = ((ulong)1) << bucketOffset;
+                var mask2 = ulong.MaxValue;
+                var mask = mask1 ^ mask2;
+
+                buckets[bucketIndex] &= mask;
+            }
+        }
+
+        public bool Get(int index)
+        {
+            if (index < 0)
+            {
+                throw new ArgumentOutOfRangeException($"The '{nameof(index)}' parameter must be at least 0.", nameof(index));
+            }
+
+            if (index >= size)
+            {
+                throw new ArgumentOutOfRangeException($"The '{nameof(index)}' parameter must not exceed the bitmap's capacity.", nameof(index));
+            }
+
+            var bucketIndex = index / bucketSize;
+            var bucketOffset = index % bucketSize;
+
+            var value = buckets[bucketIndex];
+
+            return ((value >> bucketOffset) & 1) != 0;
+        }
+    }
+}
