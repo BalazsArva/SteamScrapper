74594255144d1b7b62ba772c735a0a8ba7774e1d
Árva Balázs <balazs.arva@windowslive.com>
2021-02-27 11:47:46 +0100

    BundlePage also makes use of the pre-extracted HTML nodes.

--- src/SteamScrapper.Domain/PageModels/BundlePage.cs
+++ src/SteamScrapper.Domain/PageModels/BundlePage.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using HtmlAgilityPack;
 using SteamScrapper.Common.Constants;
+using SteamScrapper.Common.Extensions;
 using SteamScrapper.Common.Utilities.Links;
 
 namespace SteamScrapper.Domain.PageModels
@@ -11,7 +12,7 @@ namespace SteamScrapper.Domain.PageModels
         public const string UnknownBundleName = "Unknown bundle";
 
         public BundlePage(Uri address, HtmlDocument pageHtml)
-            : base(address, pageHtml)
+            : base(address, pageHtml, HtmlElements.HeaderLevel2)
         {
             if (!(address.AbsoluteUri ?? string.Empty).StartsWith(PageUrlPrefixes.Bundle, StringComparison.OrdinalIgnoreCase))
             {
@@ -30,26 +31,21 @@ namespace SteamScrapper.Domain.PageModels
 
         protected override string ExtractFriendlyName()
         {
-            var h2 = PageHtml.DocumentNode.Descendants("h2");
-            var header = h2.FirstOrDefault(x => x.Attributes.Any(a => a.Name == "class" && a.Value == "pageheader"));
+            var appNameDiv = PrefetchedHtmlNodes[HtmlElements.HeaderLevel2].FirstOrDefault(x => x.HasAttribute(HtmlAttributes.Class, "pageheader"));
 
-            return header is null ? UnknownBundleName : header.InnerText;
+            return appNameDiv is null ? UnknownBundleName : appNameDiv.InnerText;
         }
 
         private decimal ExtractPrice()
         {
-            var addToCartForm = PageHtml
-                .DocumentNode
-                .Descendants("form")
-                .Where(form => form.GetAttributeValue("name", null) == $"add_bundle_to_cart_{BundleId}")
-                .FirstOrDefault();
+            var addToCartForm = PrefetchedHtmlNodes[HtmlElements.Form].FirstOrDefault(x => x.HasAttribute(HtmlAttributes.Name, $"add_bundle_to_cart_{BundleId}"));
 
             if (addToCartForm is not null)
             {
                 // Note: this currently assumes €, unaware of currencies.
                 var finalPriceCents = addToCartForm
                     .ParentNode
-                    .Descendants("div")
+                    .GetDescendantsByNames(HtmlElements.Div)[HtmlElements.Div]
                     .Select(div => div.GetAttributeValue("data-price-final", -1))
                     .Where(finalPriceValue => finalPriceValue != -1)
                     .DefaultIfEmpty(-1)
