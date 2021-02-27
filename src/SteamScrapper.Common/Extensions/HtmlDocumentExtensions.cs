d0c2e58e11dab196b07ef15d14aa761eecf6e61f
Árva Balázs <balazs.arva@windowslive.com>
2021-02-27 11:21:50 +0100

    Created HTML extensions that allow to collect multiple kinds of nodes in one iteration.

--- src/SteamScrapper.Common/Extensions/HtmlDocumentExtensions.cs
+++ src/SteamScrapper.Common/Extensions/HtmlDocumentExtensions.cs
@@ -48,5 +48,65 @@ namespace SteamScrapper.Common.Extensions
 
             return result;
         }
+
+        public static IDictionary<string, IEnumerable<HtmlNode>> GetDescendantsByNames(this HtmlDocument doc, params string[] elementNames)
+        {
+            if (doc is null)
+            {
+                throw new ArgumentNullException(nameof(doc));
+            }
+
+            return doc.DocumentNode.GetDescendantsByNames(elementNames);
+        }
+
+        public static IDictionary<string, IEnumerable<HtmlNode>> GetDescendantsByNames(this HtmlNode htmlNode, params string[] elementNames)
+        {
+            if (htmlNode is null)
+            {
+                throw new ArgumentNullException(nameof(htmlNode));
+            }
+
+            if (elementNames is null)
+            {
+                throw new ArgumentNullException(nameof(elementNames));
+            }
+
+            if (elementNames.Length == 0)
+            {
+                throw new ArgumentException("At least one element name must be provided.", nameof(elementNames));
+            }
+
+            var result = new Dictionary<string, List<HtmlNode>>(elementNames.Length, StringComparer.Ordinal);
+
+            for (var i = 0; i < elementNames.Length; ++i)
+            {
+                var elementName = elementNames[i];
+
+                if (string.IsNullOrWhiteSpace(elementName))
+                {
+                    throw new ArgumentException("The element name cannot be null, empty or whitespace-only.", nameof(elementNames));
+                }
+
+                result[elementName] = new List<HtmlNode>(256);
+            }
+
+            var processingQueue = new Queue<HtmlNode>(1024);
+
+            processingQueue.Enqueue(htmlNode);
+
+            while (processingQueue.Count != 0)
+            {
+                var itemToProcess = processingQueue.Dequeue();
+
+                if (result.ContainsKey(itemToProcess.Name))
+                {
+                    result[itemToProcess.Name].Add(itemToProcess);
+                }
+
+                processingQueue.EnqueueRange(itemToProcess.ChildNodes);
+            }
+
+            return result.ToDictionary(x => x.Key, x => (IEnumerable<HtmlNode>)x.Value);
+        }
     }
 }
