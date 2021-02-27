03130932fa617626aa95fb63e6fe3806c4565e6c
Árva Balázs <balazs.arva@windowslive.com>
2021-02-27 14:07:01 +0100

    Project skeleton for app explorer.

---
+++ src/SteamScrapper.AppExplorer/Program.cs
@@ -0,0 +1,18 @@
+using Microsoft.Extensions.Hosting;
+
+namespace SteamScrapper.AppExplorer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                });
+    }
+}
