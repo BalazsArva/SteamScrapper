aa12aaef16e758f86931cdcba4eabfa711dce6fa
Árva Balázs <balazs.arva@windowslive.com>
2021-03-13 13:25:44 +0100

    When the crawler process finishes, it waits until the next day and restarts.

--- src/SteamScrapper.Crawler/BackgroundServices/CrawlerBackgroundService.cs
+++ src/SteamScrapper.Crawler/BackgroundServices/CrawlerBackgroundService.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SteamScrapper.Common.Providers;
 using SteamScrapper.Crawler.Commands.ExplorePage;
 using SteamScrapper.Crawler.Commands.RegisterStartingAddresses;
 
@@ -10,43 +11,65 @@ namespace SteamScrapper.Crawler.BackgroundServices
 {
     public class CrawlerBackgroundService : BackgroundService
     {
-        private readonly ILogger logger;
+        private readonly IDateTimeProvider dateTimeProvider;
         private readonly IExplorePageCommandHandler explorePageCommandHandler;
+        private readonly ILogger logger;
         private readonly IRegisterStartingAddressesCommandHandler registerStartingAddressesCommandHandler;
 
         public CrawlerBackgroundService(
-            ILogger<CrawlerBackgroundService> logger,
+            IDateTimeProvider dateTimeProvider,
             IExplorePageCommandHandler explorePageCommandHandler,
+            ILogger<CrawlerBackgroundService> logger,
             IRegisterStartingAddressesCommandHandler registerStartingAddressesCommandHandler)
         {
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this.dateTimeProvider = dateTimeProvider;
             this.explorePageCommandHandler = explorePageCommandHandler ?? throw new ArgumentNullException(nameof(explorePageCommandHandler));
+            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
             this.registerStartingAddressesCommandHandler = registerStartingAddressesCommandHandler ?? throw new ArgumentNullException(nameof(registerStartingAddressesCommandHandler));
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            await registerStartingAddressesCommandHandler.RegisterStartingAddressesAsync(stoppingToken);
-
             while (!stoppingToken.IsCancellationRequested)
             {
-                try
+                await registerStartingAddressesCommandHandler.RegisterStartingAddressesAsync(stoppingToken);
+
+                while (!stoppingToken.IsCancellationRequested)
                 {
-                    var result = await explorePageCommandHandler.ExplorePageAsync(stoppingToken);
-                    if (result == ExplorePageCommandResult.NoMoreItems)
+                    try
                     {
-                        // No links remain to explore.
-                        // TODO: Restart the next day. But check cancellation token as well.
-                        break;
+                        var result = await explorePageCommandHandler.ExplorePageAsync(stoppingToken);
+                        if (result == ExplorePageCommandResult.NoMoreItems)
+                        {
+                            logger.LogInformation("The crawler process has finished processing all links.");
+                            break;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        logger.LogError(e, "An unhandled error occurred during the crawling process.");
                     }
                 }
-                catch (Exception e)
+
+                if (stoppingToken.IsCancellationRequested)
                 {
-                    logger.LogError(e, "An unhandled error occurred during the crawling process.");
+                    break;
+                }
+
+                var tomorrowMorning = dateTimeProvider.UtcNow.Date.AddDays(1).AddMinutes(5);
+                var waitTime = tomorrowMorning - dateTimeProvider.UtcNow;
+
+                try
+                {
+                    await Task.Delay(waitTime, stoppingToken);
+                }
+                catch (TaskCanceledException)
+                {
+                    break;
                 }
             }
 
-            logger.LogInformation("Finished crawling.");
+            logger.LogInformation("Appplication shutdown requested, the crawler service has been terminated.");
         }
     }
 }
