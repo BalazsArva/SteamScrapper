5a18ea670227dd820609192971abc316892663ff
Árva Balázs <balazs.arva@windowslive.com>
2021-03-14 16:40:50 +0100

    ScanSubsBackgroundService differentiates wait periods based on the outcome. For example, it waits 5 minutes in case of a rate limit exceeded error, and only 1 minute for other errors.

--- src/SteamScrapper.SubScanner/BackgroundServices/ScanSubsBackgroundService.cs
+++ src/SteamScrapper.SubScanner/BackgroundServices/ScanSubsBackgroundService.cs
@@ -3,13 +3,15 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using SteamScrapper.Domain.Services.Exceptions;
 using SteamScrapper.SubScanner.Commands.ScanSubBatch;
 
 namespace SteamScrapper.SubScanner.BackgroundServices
 {
     public class ScanSubsBackgroundService : BackgroundService
     {
-        private const int DelaySecondsOnError = 60;
+        private const int DelaySecondsOnUnknownError = 60;
+        private const int DelaySecondsOnRateLimitExceededError = 300;
         private const int DelaySecondsOnNoMoreItems = 300;
 
         private readonly IScanSubBatchCommandHandler handler;
@@ -39,11 +41,15 @@ namespace SteamScrapper.SubScanner.BackgroundServices
                         delaySeconds = DelaySecondsOnNoMoreItems;
                     }
                 }
+                catch (SteamRateLimitExceededException e)
+                {
+                    logger.LogError(e, "The request rate limit has been exceeded while scanning a batch of subs. Retrying in {@Delay} seconds.", DelaySecondsOnRateLimitExceededError);
+                    delaySeconds = DelaySecondsOnRateLimitExceededError;
+                }
                 catch (Exception e)
                 {
-                    logger.LogError(e, "An unhandled error occurred while scanning a batch of subs. Retrying in {@Delay} seconds.", DelaySecondsOnError);
-
-                    delaySeconds = DelaySecondsOnError;
+                    logger.LogError(e, "An unhandled error occurred while scanning a batch of subs. Retrying in {@Delay} seconds.", DelaySecondsOnUnknownError);
+                    delaySeconds = DelaySecondsOnUnknownError;
                 }
 
                 if (delaySeconds > 0)
