aa6a77f9de7b5c3ebcb89969550526d467e85bd2
Árva Balázs <balazs.arva@windowslive.com>
2021-03-15 11:31:58 +0100

    CrawlerPrefetchService wraps results and rethrows exceptions for prefetch tasks. This allows the consumer to discover and handle special exceptions, such as rate limit exceeded.

--- src/SteamScrapper.Infrastructure/Services/CrawlerPrefetchService.cs
+++ src/SteamScrapper.Infrastructure/Services/CrawlerPrefetchService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using SteamScrapper.Common.Extensions;
 using SteamScrapper.Domain.Factories;
 using SteamScrapper.Domain.PageModels;
 using SteamScrapper.Domain.Services.Abstractions;
@@ -14,7 +15,8 @@ namespace SteamScrapper.Infrastructure.Services
     // Add those back to the 'to be explored' set and remove them from the 'explored' set.
     public class CrawlerPrefetchService : ICrawlerPrefetchService
     {
-        private record FetchDescriptor(Uri Uri, Task<string> HtmlDownloadTask);
+        private record FetchResult(string PageHtml, Exception Exception);
+        private record FetchDescriptor(Uri Uri, Task<FetchResult> HtmlDownloadTask);
 
         private const int PrefetchCount = 5;
 
@@ -34,48 +36,124 @@ namespace SteamScrapper.Infrastructure.Services
             this.steamPageFactory = steamPageFactory ?? throw new ArgumentNullException(nameof(steamPageFactory));
         }
 
-        public async Task<SteamPage> GetNextPageAsync(DateTime utcNow)
+        public async Task<SteamPage> GetNextPageAsync(DateTime executionDate)
         {
-            // Clean up possibly failed tasks before determining how many new ones should be launched.
-            RemoveFailedPrefetchTasks();
-
-            await PrefetchNewItemIfNeededAsync(utcNow);
+            // Populate prefetch list, this is important at first run.
+            await PrefetchNewItemsIfNeededAsync(executionDate);
 
             if (prefetchList.Count == 0)
             {
                 return null;
             }
 
-            // TODO: This will stall forever if every fetch task fails, e.g. due to network outage.
-            // Wait until any download task is completed. It will continue immediately if there's an already fetched item.
             await Task.WhenAny(prefetchList.Select(x => x.HtmlDownloadTask));
 
             // Find a completed item.
             var completedTaskIndex = prefetchList.FindIndex(x => x.HtmlDownloadTask.IsCompleted);
             var completedTask = prefetchList[completedTaskIndex];
+            var completedTaskResult = completedTask.HtmlDownloadTask.Result;
 
             prefetchList.RemoveAt(completedTaskIndex);
 
             // Add a new prefetch task to fill the place of the just removed completed fetch task.
-            await PrefetchNewItemIfNeededAsync(utcNow);
+            await PrefetchNewItemsIfNeededAsync(executionDate);
 
-            return await steamPageFactory.CreateSteamPageAsync(completedTask.Uri, completedTask.HtmlDownloadTask.Result);
+            if (completedTaskResult.Exception is not null)
+            {
+                throw completedTaskResult.Exception;
+            }
+
+            return await steamPageFactory.CreateSteamPageAsync(completedTask.Uri, completedTaskResult.PageHtml);
         }
 
-        private void RemoveFailedPrefetchTasks()
+        public async Task CancelAllReservationsAsync(DateTime executionDate)
         {
+            var uris = prefetchList.Select(x => x.Uri).ToList();
+
+            await crawlerAddressRegistrationService.UndoReservationsAsync(executionDate, uris);
+
+            prefetchList.Clear();
+        }
+
+        private bool TryGetRateLimitExceededException(out SteamRateLimitExceededException steamRateLimitExceededException)
+        {
+            for (var i = 0; i < prefetchList.Count; ++i)
+            {
+                var prefetchTask = prefetchList[i].HtmlDownloadTask;
+
+                if (prefetchTask.Status == TaskStatus.Faulted)
+                {
+                    var unwrappedExceptions = UnwrapAggregateException(prefetchTask.Exception);
+
+                    var result = unwrappedExceptions.FirstOrDefault(x => x is SteamRateLimitExceededException);
+                    if (result is not null)
+                    {
+                        steamRateLimitExceededException = (SteamRateLimitExceededException)result;
+                        return true;
+                    }
+                }
+            }
+
+            steamRateLimitExceededException = default;
+            return false;
+        }
+
+        private IEnumerable<Exception> UnwrapAggregateException(AggregateException aggregateException)
+        {
+            var result = new List<Exception>();
+            var processingQueue = new Queue<Exception>(50);
+
+            processingQueue.Enqueue(aggregateException);
+
+            while (processingQueue.Count > 0)
+            {
+                var nextException = processingQueue.Dequeue();
+
+                if (nextException is AggregateException nextAggregateException)
+                {
+                    processingQueue.EnqueueRange(nextAggregateException.InnerExceptions);
+                }
+                else
+                {
+                    result.Add(nextException);
+                }
+            }
+
+            return result;
+        }
+
+        private async Task RemoveFailedPrefetchTasksAsync(DateTime executionDate)
+        {
+            var unreserveUris = new List<Uri>(prefetchList.Count);
+
             for (var i = prefetchList.Count - 1; i >= 0; --i)
             {
-                var taskStatus = prefetchList[i].HtmlDownloadTask.Status;
+                var item = prefetchList[i];
+                var task = item.HtmlDownloadTask;
+                var taskStatus = task.Status;
 
-                if (taskStatus == TaskStatus.Canceled || taskStatus == TaskStatus.Faulted)
+                if (taskStatus == TaskStatus.Canceled)
                 {
                     prefetchList.RemoveAt(i);
                 }
+                else if (taskStatus == TaskStatus.Faulted)
+                {
+                    var unwrappedException = UnwrapAggregateException(task.Exception);
+
+                    if (unwrappedException.All(x => x is not SteamPageRemovedException))
+                    {
+                        // The error does not indicate that the page is removed, so it can be unreserved and retried later.
+                        unreserveUris.Add(item.Uri);
+                    }
+
+                    prefetchList.RemoveAt(i);
+                }
             }
+
+            await crawlerAddressRegistrationService.UndoReservationsAsync(executionDate, unreserveUris);
         }
 
-        private async Task PrefetchNewItemIfNeededAsync(DateTime utcNow)
+        private async Task PrefetchNewItemsIfNeededAsync(DateTime utcNow)
         {
             var requiredPrefetchCount = PrefetchCount - prefetchList.Count;
 
@@ -84,7 +162,7 @@ namespace SteamScrapper.Infrastructure.Services
                 var nextAddress = await crawlerAddressRegistrationService.GetNextAddressAsync(utcNow, default);
                 if (nextAddress is not null)
                 {
-                    prefetchList.Add(new(nextAddress, PrefetchAsync(nextAddress)));
+                    prefetchList.Add(new(nextAddress, DownloadPageHtmlAsync(nextAddress)));
                 }
                 else
                 {
@@ -93,16 +171,17 @@ namespace SteamScrapper.Infrastructure.Services
             }
         }
 
-        private async Task<string> PrefetchAsync(Uri address)
+        private async Task<FetchResult> DownloadPageHtmlAsync(Uri address)
         {
             try
             {
-                return await steamService.GetPageHtmlWithoutRetryAsync(address);
+                var pageHtml = await steamService.GetPageHtmlWithoutRetryAsync(address);
+
+                return new(pageHtml, null);
             }
-            catch (SteamRateLimitExceededException)
+            catch (Exception e)
             {
-                // TODO: Put the URL back to exploration queue
-                throw;
+                return new(null, e);
             }
         }
     }
