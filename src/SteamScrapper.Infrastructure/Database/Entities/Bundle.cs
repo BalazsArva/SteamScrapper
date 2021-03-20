e3e9ad2361a4a6ccc2f2cce8ad85fc315ac74bcf
Árva Balázs <balazs.arva@windowslive.com>
2021-03-20 12:32:20 +0100

    Entities for DB.

---
+++ src/SteamScrapper.Infrastructure/Database/Entities/Bundle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SteamScrapper.Infrastructure.Database.Entities
+{
+    public class Bundle
+    {
+        public long Id { get; set; }
+
+        public DateTime UtcDateTimeRecorded { get; set; }
+
+        public DateTime UtcDateTimeLastModified { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string Title { get; set; }
+
+        public string BannerUrl { get; set; }
+    }
+}
---
+++ src/SteamScrapper.Infrastructure/Database/Entities/Sub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SteamScrapper.Infrastructure.Database.Entities
+{
+    public class Sub
+    {
+        public long Id { get; set; }
+
+        public DateTime UtcDateTimeRecorded { get; set; }
+
+        public DateTime UtcDateTimeLastModified { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public string Title { get; set; }
+    }
+}
