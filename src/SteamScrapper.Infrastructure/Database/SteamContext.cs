5666fe6665845b5ee6dc1515cf70b1f1a4930246
Árva Balázs <balazs.arva@windowslive.com>
2021-03-20 12:40:50 +0100

    WIP on db context.

---
+++ src/SteamScrapper.Infrastructure/Database/SteamContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SteamScrapper.Infrastructure.Database.Entities;
+
+namespace SteamScrapper.Infrastructure.Database
+{
+    public class SteamContext : DbContext
+    {
+        public SteamContext(DbContextOptions<SteamContext> dbContextOptions)
+            : base(dbContextOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder
+                .Entity<App>()
+                .HasKey(x => x.Id);
+
+            modelBuilder
+                .Entity<App>()
+                .Property(x => x.UtcDateTimeRecorded)
+                .HasDefaultValueSql("SYSUTCDATETIME()");
+
+            modelBuilder
+                .Entity<App>()
+                .Property(x => x.UtcDateTimeRecorded)
+                .HasDefaultValueSql("SYSUTCDATETIME()");
+
+            modelBuilder
+                .Entity<App>()
+                .Property(x => x.UtcDateTimeLastModified)
+                .HasDefaultValueSql("CONVERT(datetime2(7), N'2000-01-01T00:00:00+00:00')");
+        }
+    }
+}
