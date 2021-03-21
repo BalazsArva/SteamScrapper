e11b8059da571c2a8ecf764c0d02b4ca79ac5093
Árva Balázs <balazs.arva@windowslive.com>
2021-03-21 12:13:17 +0100

    Created AppRepository.

---
+++ src/SteamScrapper.Domain/Repositories/IAppRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SteamScrapper.Domain.Repositories
+{
+    public interface IAppRepository
+    {
+        Task<int> RegisterUnknownAppsAsync(IEnumerable<int> appIds);
+    }
+}
---
+++ src/SteamScrapper.Infrastructure/Database/Repositories/AppRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using SteamScrapper.Domain.Repositories;
+using SteamScrapper.Infrastructure.Database.Context;
+
+namespace SteamScrapper.Infrastructure.Database.Repositories
+{
+    public class AppRepository : IAppRepository
+    {
+        private readonly SteamContext context;
+
+        public AppRepository(SteamContext steamContext)
+        {
+            context = steamContext ?? throw new ArgumentNullException(nameof(steamContext));
+        }
+
+        public async Task<int> RegisterUnknownAppsAsync(IEnumerable<int> appIds)
+        {
+            if (appIds is null)
+            {
+                throw new ArgumentNullException(nameof(appIds));
+            }
+
+            return await context.RegisterUnknownAppsAsync(appIds);
+        }
+    }
+}
