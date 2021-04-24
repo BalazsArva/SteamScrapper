9aa9f245929ae49033004e4bac135e7b77a94fbc
Árva Balázs <balazs.arva@windowslive.com>
2021-04-24 11:26:39 +0200

    Added RavenDbHealthCheck.

---
+++ src/SteamScrapper.Infrastructure/RavenDb/RavenDbHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Raven.Client.Documents;
+using Raven.Client.Documents.Operations;
+
+namespace SteamScrapper.Infrastructure.RavenDb
+{
+    public class RavenDbHealthCheck : IHealthCheck
+    {
+        private static readonly GetStatisticsOperation GetStatisticsOperation = new GetStatisticsOperation();
+
+        private readonly IDocumentStore documentStore;
+
+        public RavenDbHealthCheck(IDocumentStore documentStore)
+        {
+            this.documentStore = documentStore ?? throw new ArgumentNullException(nameof(documentStore));
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                _ = await documentStore.Maintenance.SendAsync(GetStatisticsOperation, cancellationToken);
+
+                return HealthCheckResult.Healthy("Successfully checked RavenDB health.");
+            }
+            catch (Exception e)
+            {
+                return HealthCheckResult.Unhealthy("Failed to get response from RavenDB.", e);
+            }
+        }
+    }
+}
