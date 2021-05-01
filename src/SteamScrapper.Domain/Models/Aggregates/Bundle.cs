7c7eec2a8bd76289131ca510b6161aad4155b373
Árva Balázs <balazs.arva@windowslive.com>
2021-05-01 12:19:05 +0200

    Repositorry for bundle aggregates.

---
+++ src/SteamScrapper.Domain/Models/Aggregates/Bundle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace SteamScrapper.Domain.Models.Aggregates
+{
+    public class Bundle
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string BannerUrl { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public List<PriceHistory> PriceHistoryByCurrency { get; } = new List<PriceHistory>();
+    }
+}
---
+++ src/SteamScrapper.Infrastructure/RavenDb/Repositories/BundleAggregateRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SteamScrapper.Domain.Models.Aggregates;
+using SteamScrapper.Domain.Repositories;
+
+namespace SteamScrapper.Infrastructure.RavenDb.Repositories
+{
+    public class BundleAggregateRepository : IBundleAggregateRepository
+    {
+        private readonly IDocumentStoreWrapper documentStoreWrapper;
+
+        public BundleAggregateRepository(IDocumentStoreWrapper documentStoreWrapper)
+        {
+            this.documentStoreWrapper = documentStoreWrapper ?? throw new ArgumentNullException(nameof(documentStoreWrapper));
+        }
+
+        public async Task StoreBundleAggregatesAsync(IEnumerable<Bundle> bundles)
+        {
+            if (bundles is null)
+            {
+                throw new ArgumentNullException(nameof(bundles));
+            }
+
+            if (!bundles.Any())
+            {
+                return;
+            }
+
+            using var session = documentStoreWrapper.DocumentStore.OpenAsyncSession();
+
+            foreach (var bundle in bundles)
+            {
+                await session.StoreAsync(bundle, bundle.Id);
+            }
+
+            await session.SaveChangesAsync();
+        }
+    }
+}
